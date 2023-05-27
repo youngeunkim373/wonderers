5de1f133a60273015cc1e93564da83a9f3f9ef18
cherie1214 <cheriey1214@gmail.com>
2023-05-27 16:47:30 +0900

    폴더 구조 및 이미지 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 
 // if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
-import('../mocks');
+import("../mocks");
 // }
 
 export default function App({ Component, pageProps }: AppProps) {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
 import { Button, Text } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { getTestApi, postTestApi } from "@/domain/test/test.service";
@@ -13,7 +12,6 @@ interface Props {
 }
 
 export default function Home({ data }: Props) {
-  console.log(data);
   const [testData, setTestData] = useState<any>([]);
 
   const postTest = async () => {
@@ -26,10 +24,6 @@ export default function Home({ data }: Props) {
     setTestData(result?.data);
   };
 
-  useEffect(() => {
-    setTestData(data);
-  }, []);
-
   return (
     <>
       <Head>
@@ -38,13 +32,12 @@ export default function Home({ data }: Props) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.center}>
+      <main>
+        <div>
           <Text fontSize="md" mb="50px">
             Welcome, We are
           </Text>
           <Image
-            className={styles.logo}
             src="/logo.svg"
             alt="Next.js Logo"
             width={400}
