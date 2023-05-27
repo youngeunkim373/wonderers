f9f947d5a1c7f5cb7b77bfbfd7eb7c47c5f19f67
cherie1214 <cheriey1214@gmail.com>
2023-05-27 16:56:23 +0900

    trivial: index 페이지 누락된 코드 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,33 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import { Text, Box, Link, Button } from "@chakra-ui/react";
+import { Button, Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { getTestApi, postTestApi } from "@/domain/test/test.service";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+interface Props {
+  data: any[];
+}
+
+export default function Home({ data }: Props) {
+  const [testData, setTestData] = useState<any>([]);
+
+  const postTest = async () => {
+    const result = await postTestApi({
+      id: "1234",
+      name: "lee",
+      country: "ko",
+      lang: "react",
+    });
+    setTestData(result?.data);
+  };
+
+  useEffect(() => {
+    setTestData(data);
+  }, []);
+
   return (
     <>
       <Head>
@@ -21,13 +43,14 @@ export default function Home() {
             Welcome, We are
           </Text>
           <Image
+            className={styles.logo}
             src="/logo.svg"
             alt="Next.js Logo"
             width={400}
             height={70}
             priority
           />
-          <Button mt="50px" colorScheme="blue">
+          <Button mt="50px" colorScheme="blue" onClick={postTest}>
             Button
           </Button>
 
