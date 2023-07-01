b513bf62f866e65bf1010ead7c71588bb9709022
0=2 dev <104338021+youngeunkim373@users.noreply.github.com>
2023-07-01 17:09:56 +0900

    fix: 빌드에러 해결 (#9)
    Co-authored-by: Youngeuninswing <121157446+Youngeuninswing@users.noreply.github.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,11 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
-import customTheme from "../system/chakraTheme";
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+import customTheme from '../system/chakraTheme';
 
-// if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
-import("../mocks");
-// }
+if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
+  import('../mocks');
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
--- src/system/chakraTheme/components/button.ts
+++ src/system/chakraTheme/components/button.ts
@@ -1,126 +1,123 @@
-import type {
-  SystemStyleObject,
-  SystemStyleFunction,
-} from "@chakra-ui/styled-system";
+import type { SystemStyleObject, SystemStyleFunction } from '@chakra-ui/styled-system';
 
 const baseStyle: SystemStyleObject = {
-  fontWeight: "normal",
-  fontFamily: "body",
-  borderRadius: "0.4rem",
+  fontWeight: 'normal',
+  fontFamily: 'body',
+  borderRadius: '0.4rem',
 };
 
 const variantLink: SystemStyleFunction = (props) => {
   const { colorScheme: c } = props;
 
-  if (c === "black") {
+  if (c === 'black') {
     return {
-      color: "black.main",
+      color: 'black.main',
     };
   }
-  if (c === "gray") {
+  if (c === 'gray') {
     return {
-      color: "black.sub",
+      color: 'black.sub',
     };
   }
-  if (c === "pink") {
+  if (c === 'pink') {
     return {
-      color: "pink.main",
+      color: 'pink.main',
     };
   }
 
   return {
-    color: "black.main",
+    color: 'black.main',
   };
 };
 
 const variantGhost: SystemStyleFunction = (props) => {
   const { colorScheme: c } = props;
 
-  if (c === "black") {
+  if (c === 'black') {
     return {
-      color: "black.main",
+      color: 'black.main',
       _hover: {
-        bg: "gray.200",
+        bg: 'gray.200',
       },
     };
   }
-  if (c === "gray") {
+  if (c === 'gray') {
     return {
-      color: "black.sub",
+      color: 'black.sub',
     };
   }
-  if (c === "pink") {
+  if (c === 'pink') {
     return {
-      color: "pink.main",
+      color: 'pink.main',
     };
   }
 
   return {
-    color: "black.main",
+    color: 'black.main',
   };
 };
 
 const variantOutline: SystemStyleFunction = (props) => {
   const { colorScheme: c } = props;
 
-  if (c === "black") {
+  if (c === 'black') {
     return {
-      bg: "white",
-      borderColor: "black.main",
-      color: "black.main",
+      bg: 'white',
+      borderColor: 'black.main',
+      color: 'black.main',
       _hover: {
-        bg: "black.main",
-        color: "white",
+        bg: 'black.main',
+        color: 'white',
         _disabled: {
-          bg: "black.third",
+          bg: 'black.third',
         },
       },
-      _active: { bg: "primary-dark" },
+      _active: { bg: 'primary-dark' },
     };
   }
 
-  if (c === "gray") {
+  if (c === 'gray') {
     return {
-      bg: "white",
-      borderColor: "black.sub",
-      color: "black.sub",
+      bg: 'white',
+      borderColor: 'black.sub',
+      color: 'black.sub',
       _hover: {
-        bg: "black.sub",
-        color: "white",
+        bg: 'black.sub',
+        color: 'white',
         _disabled: {
-          bg: "black.third",
+          bg: 'black.third',
         },
       },
-      _active: { bg: "primary-dark" },
+      _active: { bg: 'primary-dark' },
     };
   }
 
-  if (c === "pink") {
+  if (c === 'pink') {
     return {
-      bg: "white",
-      borderColor: "pink.main",
-      color: "pink.main",
+      bg: 'white',
+      borderColor: 'pink.main',
+      color: 'pink.main',
       _hover: {
-        bg: "pink.main",
-        color: "white",
+        bg: 'pink.main',
+        color: 'white',
         _disabled: {
-          bg: "pink.third",
+          bg: 'pink.third',
         },
       },
-      _active: { bg: "primary-dark" },
+      _active: { bg: 'primary-dark' },
     };
   }
 
   return {
-    border: "1px solid",
-    borderColor: c === "warning" ? "info-increase" : "gray.400",
-    color: "gray.900",
-    bg: "white",
+    border: '1px solid',
+    borderColor: c === 'warning' ? 'info-increase' : 'gray.400',
+    color: 'gray.900',
+    bg: 'white',
     _hover: {
-      bg: "gray.100",
+      bg: 'gray.100',
     },
     _active: {
-      bg: "gray.100",
+      bg: 'gray.100',
     },
   };
 };
@@ -128,58 +125,58 @@ const variantOutline: SystemStyleFunction = (props) => {
 const variantSolid: SystemStyleFunction = (props) => {
   const { colorScheme: c } = props;
 
-  if (c === "black-solid") {
+  if (c === 'black-solid') {
     return {
-      bg: "black",
-      color: "white",
+      bg: 'black',
+      color: 'white',
       _hover: {
-        bg: "pink.main",
+        bg: 'pink.main',
         _disabled: {
-          bg: "black.third",
+          bg: 'black.third',
         },
       },
-      _active: { bg: "pink.main" },
+      _active: { bg: 'pink.main' },
     };
   }
 
-  if (c === "gray") {
+  if (c === 'gray') {
     return {
-      bg: "black.sub",
-      color: "white",
+      bg: 'black.sub',
+      color: 'white',
       _hover: {
-        bg: "gray.800",
+        bg: 'gray.800',
         _disabled: {
-          bg: "gray.300",
+          bg: 'gray.300',
         },
       },
-      _active: { bg: "gray.800" },
+      _active: { bg: 'gray.800' },
     };
   }
 
-  if (c === "pink") {
+  if (c === 'pink') {
     return {
-      bg: "pink.main",
-      color: "white",
+      bg: 'pink.main',
+      color: 'white',
       _hover: {
-        bg: "pink.dark",
+        bg: 'pink.dark',
         _disabled: {
-          bg: "gray.300",
+          bg: 'gray.300',
         },
       },
-      _active: { bg: "black.main" },
+      _active: { bg: 'black.main' },
     };
   }
 
   return {
-    bg: "black",
-    color: "white",
+    bg: 'black',
+    color: 'white',
     _hover: {
-      bg: "pink.main",
+      bg: 'pink.main',
       _disabled: {
-        bg: "black.third",
+        bg: 'black.third',
       },
     },
-    _active: { bg: "pink.main" },
+    _active: { bg: 'pink.main' },
   };
 };
 
@@ -195,34 +192,36 @@ const sizes: Record<string, SystemStyleObject> = {
   lg: {
     h: 12,
     minW: 12,
-    fontSize: "h6",
+    fontSize: 'h6',
     px: 7,
     py: 5,
   },
   md: {
     h: 10,
     minW: 10,
-    fontSize: "p",
+    fontSize: 'p',
     px: 4,
   },
   sm: {
     h: 8,
     minW: 8,
-    fontSize: "small",
+    fontSize: 'small',
     px: 3,
   },
   //xs: null,
 };
 
 const defaultProps = {
-  variant: "solid",
-  size: "md",
-  colorScheme: "black",
+  variant: 'solid',
+  size: 'md',
+  colorScheme: 'black',
 };
 
-export default {
-  baseStyle,
+const Button = {
+  baseStyle: ``,
   variants,
   sizes,
-  defaultProps,
+  defaultProps: defaultProps,
 };
+
+export default Button;
