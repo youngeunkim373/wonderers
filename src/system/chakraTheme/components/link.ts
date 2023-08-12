029bee87b85879fb959e4a4aa5386357049de242
Youngeuninswing <121157446+Youngeuninswing@users.noreply.github.com>
2023-08-12 16:54:02 +0900

    feat: 차크라 링크 컴포넌트 커스텀

---
+++ src/system/chakraTheme/components/link.ts
@@ -0,0 +1,14 @@
+import type { SystemStyleObject } from '@chakra-ui/styled-system';
+
+const baseStyle: SystemStyleObject = {
+  _hover: {
+    color: 'MainColor',
+    textDecoration: 'none',
+  },
+};
+
+const Link = {
+  baseStyle,
+};
+
+export default Link;
