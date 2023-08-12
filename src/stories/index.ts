11a632020f6fb18e9d168b8b7b919c5b50c083ff
cherie1214 <cheriey1214@gmail.com>
2023-08-12 16:09:03 +0900

    fix: build 오류 수정

--- src/stories/index.ts
+++ src/stories/index.ts
@@ -5,10 +5,10 @@ import { pageTitle } from "./PageTitle";
 import { heading } from "./Heading";
 import { formControl } from "./FormControl";
 import {
-  formInput,
-  formInputWithButton,
-  formInputInvalid,
-  formInputPassword,
+  FormControlInput,
+  FormControlInputWithButton,
+  FormControlInputInvalid,
+  FormControlInputPassword,
 } from "./FormInput";
 
 export const Button = button;
@@ -17,7 +17,7 @@ export const Input = input;
 export const PageTitle = pageTitle;
 export const Heading = heading;
 export const FormControl = formControl;
-export const FormInput = formInput;
-export const FormInputWithButton = formInputWithButton;
-export const FormInputInvalid = formInputInvalid;
-export const FormInputPassword = formInputPassword;
+export const FormInput = FormControlInput;
+export const FormInputWithButton = FormControlInputWithButton;
+export const FormInputInvalid = FormControlInputInvalid;
+export const FormInputPassword = FormControlInputPassword;
