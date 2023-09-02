e1b52c7bb017185d641d92516b05b726c5e9fba5
cherie1214 <cheriey1214@gmail.com>
2023-09-02 15:56:03 +0900

    주석 추가

--- src/mocks/data/test.ts
+++ src/mocks/data/test.ts
@@ -1,9 +1,9 @@
-const UNKNOWN_DATA = "UNKNOWN";
+const UNKNOWN_DATA = 'UNKNOWN';
 
 export const mockedTest: any[] = Array(3).fill({
-  id: "user",
-  pw: "1234",
-  name: "lee",
-  country: "ko",
-  lang: "react",
+  id: 'user',
+  pw: '1234', // 추가: 로그인 테스트 시 필요
+  name: 'lee',
+  country: 'ko',
+  lang: 'react',
 });
