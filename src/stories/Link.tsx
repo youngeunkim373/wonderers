cb129226a729cab8ebd39c86fb63cf39d78d9e05
cherie1214 <cheriey1214@gmail.com>
2023-09-16 15:45:10 +0900

    Link 수정

--- src/stories/Link.tsx
+++ src/stories/Link.tsx
@@ -1,9 +1,5 @@
 import { Link, LinkProps } from '@chakra-ui/react';
 
-interface Link extends LinkProps {
-  children: React.ReactNode;
-  colorScheme?: string;
-}
 export const link = ({ children, ...props }: LinkProps) => {
   const colorMode =
     props.colorScheme === 'black'
@@ -17,7 +13,7 @@ export const link = ({ children, ...props }: LinkProps) => {
   return (
     <Link
       _hover={{ textDecoration: 'none' }}
-      fontSize={props.fontSize || 'md'}
+      fontSize={props.fontSize ?? 'md'}
       {...colorMode}
       {...props}
     >
